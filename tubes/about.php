96782f80ea14b565003a35e2c743d22026b73426
trifine <trifine.laurensi08@gmail.com>
2021-12-07 21:54:39 +0700

    mengupload about us (tugas dan peran masing-masing dalam tim)

---
+++ tubes/about.php
@@ -0,0 +1,211 @@
+<?php
+//require_once'../include/koneksi.php'
+require_once 'header.php';
+?>
+
+  <!--Main Content-->
+    <div class="site-blocks-cover inner-page" style="background-image: url('images/hero_1.jpg');">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 mx-auto align-self-center">
+            <div class=" text-center">
+              <h1>About Us</h1>
+              <p>Roxy Pharma is a pharmaceutical producer and distributor located in Indonesia. we provide medicine, pills, vitamins, and masks. 
+                We, at Roxy Pharma, strive to cure sickness and help others who cannot.</p>ˀ
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-light custom-border-bottom" data-aos="fade">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-6">
+            <div class="block-16">
+              <figure>
+                <img src="images/bg_1.jpg" alt="Image placeholder" class="img-fluid rounded">
+                <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-button popup-vimeo"><span
+                    class="icon-play"></span></a>
+    
+              </figure>
+            </div>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-5">
+    
+    
+            <div class="site-section-heading pt-3 mb-4">
+              <h2 class="text-black">OUR MISSION</h2>
+            </div>
+            <p>As the voice of pharmacy, the American Pharmacists Association leads the profession and equips members for their role as 
+              the medication expert in team-based, patient-centered care.</p>
+            <p>Roxy Pharma inspires, innovates, and creates opportunities for members and pharmacists 
+              worldwide to optimize medication use and health for all.</p>
+    
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
+    <div class="site-section bg-light custom-border-bottom" data-aos="fade">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-6 order-md-2">
+            <div class="block-16">
+              <figure>
+                <img src="images/hero_1.jpg" alt="Image placeholder" class="img-fluid rounded">
+                <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-button popup-vimeo"><span
+                    class="icon-play"></span></a>
+    
+              </figure>
+            </div>
+          </div>
+          <div class="col-md-5 mr-auto">
+    
+    
+            <div class="site-section-heading pt-3 mb-4">
+              <h2 class="text-black">WHY US?</h2>
+            </div>
+            <p class="text-black">Our expert staff, and strong volunteer leadership, including many experienced pharmacists, 
+              allow us to deliver vital leadership to help pharmacists, pharmaceutical scientists, student pharmacists and pharmacy technicians 
+              find success and satisfaction in their work, while advocating for changes that benefit them and their patients.</p>
+            <p class="text-black">RoxyPharna advocates at the national level to give pharmacists a voice in decisions that affect their 
+              future while promoting their undeniable value to health care teams. Our goal is to facilitate pharmacists’ delivery 
+              of the highest quality patient-centered care.</p>
+    
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="site-section site-section-sm site-blocks-1 border-0" data-aos="fade">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="">
+            <div class="icon mr-4 align-self-start">
+              <span class="icon-truck text-primary"></span>
+            </div>
+            <div class="text">
+              <h2>Free Shipping</h2>
+              <p>Free shipping worldwide on orders above $100 or purchases with RoxyGift Cash</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon mr-4 align-self-start">
+              <span class="icon-refresh2 text-primary"></span>
+            </div>
+            <div class="text">
+              <h2>Free Returns</h2>
+              <p>We offer solutions to help you fix what’s wrong, so you can keep and love the things you buy. Because sometimes no return is the easiest return of all.</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon mr-4 align-self-start">
+              <span class="icon-help text-primary"></span>
+            </div>
+            <div class="text">
+              <h2>Customer Support</h2>
+              <p>We’ll walk you through fixing most things here, or connect you to someone if you need more help.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+    <div class="site-section bg-light custom-border-bottom" data-aos="fade">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 site-section-heading text-center pt-4">
+            <h2>The Team</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-lg-6 mb-5">
+    
+            <div class="block-38 text-center">
+              <div class="block-38-img">
+                <div class="block-38-header">
+                  <img src="images/person_1.jpg" alt="Image placeholder" class="mb-4">
+                  <h3 class="block-38-heading h4">Vita Sariani </h3>
+                  <p class="block-38-subheading">CEO/Co-Founder</p>
+                </div>
+                <div class="block-38-body">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio
+                    recusandae doloribus ut fugit officia voluptate soluta. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-6 mb-5">
+            <div class="block-38 text-center">
+              <div class="block-38-img">
+                <div class="block-38-header">
+                  <img src="images/person_2.jpg" alt="Image placeholder" class="mb-4">
+                  <h3 class="block-38-heading h4">Jennifer Greive</h3>
+                  <p class="block-38-subheading">Co-Founder</p>
+                </div>
+                <div class="block-38-body">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio
+                    recusandae doloribus ut fugit officia voluptate soluta. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-6 mb-5">
+            <div class="block-38 text-center">
+              <div class="block-38-img">
+                <div class="block-38-header">
+                  <img src="images/person_3.jpg" alt="Image placeholder" class="mb-4">
+                  <h3 class="block-38-heading h4">Patrick Marx</h3>
+                  <p class="block-38-subheading">Marketing</p>
+                </div>
+                <div class="block-38-body">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio
+                    recusandae doloribus ut fugit officia voluptate soluta. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-6 mb-5">
+            <div class="block-38 text-center">
+              <div class="block-38-img">
+                <div class="block-38-header">
+                  <img src="images/person_4.jpg" alt="Image placeholder" class="mb-4">
+                  <h3 class="block-38-heading h4">Mike Coolbert</h3>
+                  <p class="block-38-subheading">Sales Manager</p>
+                </div>
+                <div class="block-38-body">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio
+                    recusandae doloribus ut fugit officia voluptate soluta. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+          </div>
+        </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+
+<?php
+include('footer.php');
+?>
