c4e0fc926fbfb64846e2f18dd12bf12d51a4927e
trifine <95034243+trifine@users.noreply.github.com>
2021-12-15 02:54:04 +0700

    Update index.php
    mengupdate tampilan review

--- pos-kasir-php-master/admin/module/review/index.php
+++ pos-kasir-php-master/admin/module/review/index.php
@@ -8,52 +8,52 @@
           <section class="wrapper">
 
              
-          <div class="row">
+          <div class="card-body">
                   <div class="col-lg-12 main-chart">
 						<h3>Review Product</h3>
 						<br>
-            <div class="card-body">
-                    <form class="needs-validation" novalidate method="POST" action="../login/user.php">
-                    <h5 class="modal-title">Review Product</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <div class="form-row">
-                    
-                        <div class="col-md-12 mb-3">
-                            <label for="nama">Nama </label>
-                            <input type="text" class="form-control" id="nama" value="" name="nama" placeholder="Masukkan Nama Anda" required>
-                            <div class="invalid-feedback">
-                                Silahkan Isi Nama Anda
-                            </div>
-                        </div>
-                        <div class="col-md-12 mb-3">
-                            <label for="nm_product">Nama Product</label>
-                            <input type="text" class="form-control" value="" name="nm_product" id="nm_product" placeholder="Nama product" required>
-                            <div class="invalid-feedback">
-                                Silahkan Isi Nama Product
-                            </div>
-                        </div>
-                        <div class="col-md-12 mb-3">
-                        <label for="email">Email</label>
-                            <input type="text" class="form-control" value="" name="email" id="email" placeholder="example@gmail.com" required>
-                            <div class="invalid-feedback">
-                                Silahkan Isi Email Anda
-                            </div>
-                        </div>
+            <div class="form-row">
+            <form role="form" class="form-horizontal" action="simpanuser.php" method="post">
+                        <div class="col-md-12 mb-3">
+                            <label class="col-md-12 mb-3" for="nama">Nama</label>
+                            <input type="text" name="username" class="form-control" id="nama" value="" name="nama" placeholder="Masukkan Nama Anda" required oninvalid="this.setCustomValidity('Silahkan Isi Nama Anda')" oninput="setCustomValidity('')">
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <label class="col-md-12 mb-3" for="nm_product">Nama Product</label>
+                            <input type="text" name="password" class="form-control" name="nm_product" id="nm_product" placeholder="Nama product" required oninvalid="this.setCustomValidity('Silahkan Isi Nama Product')" oninput="setCustomValidity('')">
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <label class="col-md-12 mb-3" for="email">Email</label>
+                            <input type="email" name="nama_lengkap" class="form-control"  name="email" id="email" placeholder="example@gmail.com" required oninvalid="this.setCustomValidity(' Silahkan Isi Email Anda')" oninput="setCustomValidity('')">
+                        </div>
+                    <div class="col-md-12 mb-3">
+                        <label class="col-md-12 mb-3" for="keterangan">Komentar</label>
+                        <textarea class="form-control" id="keterangan" name="keterangan" placeholder="silahkan tambahkan keterangan" required oninvalid="this.setCustomValidity(' Silahkan Isi Email Anda')" oninput="setCustomValidity('')"></textarea>
                     </div>
-                    <div class="form-group ml-3">
-                        <label for="keterangan">Komentar</label>
-                        <textarea class="form-control" id="keterangan" name="keterangan" required></textarea>
-                        <div class="invalid-feedback">
-                            silahkan tambahkan keterangan
-                        </div>
-                    </div>
-       
-                    <button name="btnUbah" type="submit" class="btn btn-primary mt-3">Save</button>
-                    <a href="index.php" class="btn btn-secondary mt-3">Close</a>
+                    <div class="col-md-12 mb-3">
+                            <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-primary" name="submitted" value="simpan">Save</button>
+                            <button type="reset" class="btn btn-secondary" onclick="self.history.back()">Close</button>
+                            </div>
+                        </div>
               </form>
-						<div class="clearfix" style="padding-top:41%;"></div>
+              <?php 
+                     if(isset($_POST['btnUbah'])) {      
+                        //$no = $_POST['id'];
+                        $nama = $_POST['nama'];
+                        $product = $_POST['nm_product'];
+                        $email = $_POST['email'];
+                        $keterangan = $_POST['keterangan'];
+                        if($conn)
+                        {
+                            $sql = "INSERT INTO `review` (`id`,  `nama`, `keterangan`, `nm_product`) VALUES (NULL, '".$nama."', '".$keterangan."', '".$product."');";
+                            mysqli_query($conn, $sql);
+                            echo "<p class='alert alert-success text-center mt-2'><b>Perubahahn data berhasil. <a href='coment.php' class='btn btn-primary'> Lihat</a></b></p>";
+                        } elseif ($conn->connect_error){
+                            echo "<p clas='alert alert-danger text-center'><b>Terjadi kesalahan: $error</b></p>";
+                        }
+                     }
+                ?>
 				  </div>
               </div>
             </div>
