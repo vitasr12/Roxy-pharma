93c8657598ce37bcf4922d90cb34ef715ed16342
vita sariani <90192195+vitasr12@users.noreply.github.com>
2021-12-24 11:45:20 +0700

    Delete pos-kasir-php-master directory

--- pos-kasir-php-master/assets/tinymce/modules/alloy/src/main/ts/ephox/alloy/behaviour/reflecting/ReflectingSchema.ts
+++
@@ -1,8 +0,0 @@
-import { FieldSchema } from '@ephox/boulder';
-
-export default [
-  FieldSchema.required('channel'),
-  FieldSchema.option('renderComponents'),
-  FieldSchema.option('updateState'),
-  FieldSchema.option('initialData')
-];
